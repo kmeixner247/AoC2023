4d4f6a0120a690ccd9b1bd7ae45f4cd491235961
konst <konstantin.meixner@freenet.de>
2023-12-22 12:17:23 +0100

    day 22

---
+++ 22/script2.py
@@ -0,0 +1,78 @@
+# look at this marvel of efficiency, it only takes like an hour to run >.>
+
+import functools
+import copy
+
+def fall(bricks, i):
+	while(not is_brick_supported(bricks[i], bricks[:i])):
+		for idx in range(len(bricks[i])):
+			bricks[i][idx][2] -= 1
+
+def is_brick_supported(brick, bricks):
+	if lowest_z(brick) > 1 and all(not is_coord_supported(coord, bricks) for coord in brick):
+		return False
+	return True
+
+def is_coord_supported(coord, bricks):
+	supporting_coord = [coord[0], coord[1], coord[2] - 1]
+	for brick in bricks:
+		if supporting_coord in brick:
+			return True
+	return False
+
+def lowest_z(a):
+	z = a[0][2]
+	for coord in a:
+		if coord[2] < z:
+			z = coord
+	return z
+
+def compare(a, b):
+	if lowest_z(a) > lowest_z(b):
+		return 1
+	if lowest_z(a) < lowest_z(b):
+		return -1
+	return 0
+
+def find_unsupported_brick(bricks):
+	for idx, brick in enumerate(bricks):
+		if not is_brick_supported(brick, bricks[:idx]):
+			return idx
+	return -1
+
+
+lines = open("input", "r").read().split("\n")
+
+bricks = list()
+
+
+for line in lines:
+	bounds = [[int(n) for n in bound.split(',')] for bound in line.split('~')]
+	brick = list()
+	for x in range(bounds[0][0], bounds[1][0] + 1):
+		for y in range(bounds[0][1], bounds[1][1] + 1):
+			for z in range(bounds[0][2], bounds[1][2] + 1):
+				brick.append([x, y, z])
+
+	bricks.append(brick)
+
+bricks = sorted(bricks, key = functools.cmp_to_key(compare))
+first_unsupported = find_unsupported_brick(bricks)
+
+while first_unsupported != -1:
+	fall(bricks, first_unsupported)
+	first_unsupported = find_unsupported_brick(bricks)
+
+count = 0
+for i in range(len(bricks)):
+	bricks_copy = copy.deepcopy(bricks)
+	bricks_copy.pop(i)
+	first_unsupported = find_unsupported_brick(bricks_copy)
+
+	while first_unsupported != -1:
+		count += 1
+		fall(bricks_copy, first_unsupported)
+		first_unsupported = find_unsupported_brick(bricks_copy)
+
+
+print(count)
